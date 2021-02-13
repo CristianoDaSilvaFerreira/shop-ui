529c8c3bce6f83348ab59b4a0f0c93904c64bc0a
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-02-13 18:13:33 -0300

    Criação do meu Top

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,12 @@ class HomePage extends StatelessWidget {
           children: [
             Container(
               height: 300,
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                left: 25,
+                right: 25,
+                top: 50,
+              ),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                   bottomRight: Radius.circular(50),
@@ -30,6 +36,54 @@ class HomePage extends StatelessWidget {
                     Color.fromRGBO(255, 250, 182, 1),
                     Color.fromRGBO(255, 239, 78, 1),
                   ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/menu.png',
+                    width: 20,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          "Melhores coleções \nde meias online",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(97, 90, 90, 1),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Image.asset(
+                          'assets/header-socks.png',
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(0, -35),
+              child: Container(
+                height: 60,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
                 ),
               ),
             )
