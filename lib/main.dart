4b3a061237a1f52a49c93603da3f66317663a568
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-02-14 23:32:30 -0300

    Atualizando o layout com...
     - Criação com do campo de pesquisa
     - Criação do menu de categorias
    Alduto e Criança
     - Criação do cart de fundo dos itens

--- lib/main.dart
+++ lib/main.dart
@@ -78,18 +78,123 @@ class HomePage extends StatelessWidget {
               offset: Offset(0, -35),
               child: Container(
                 height: 60,
-                margin: EdgeInsets.symmetric(
-                  horizontal: 25,
+                padding: EdgeInsets.only(
+                  left: 20,
+                  top: 8,
                 ),
+                margin: EdgeInsets.symmetric(horizontal: 25),
                 decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[350],
+                      blurRadius: 20,
+                      offset: Offset(0, 10),
+                    )
+                  ],
                   borderRadius: BorderRadius.circular(5),
                   color: Colors.white,
                 ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Pesquisar...',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Fecha \na category',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromRGBO(97, 90, 90, 1),
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            FlatButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(251, 51, 105, 0.1),
+                              child: Text(
+                                'Adulto',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(251, 53, 105, 1),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            FlatButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(97, 90, 90, 0.1),
+                              child: Text(
+                                'Criança',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(97, 90, 90, 0.6),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ], // <Widget>[]
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 280,
+              width: double.infinity,
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 20, left: 20),
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  makeCard(
+                    context: context,
+                    startColor: Color.fromRGBO(251, 121, 155, 1),
+                    endColor: Color.fromRGBO(251, 53, 105, 1),
+                    image: 'assets/sockes-one.png',
+                  ),
+                  makeCard(
+                    context: context,
+                    startColor: Color.fromRGBO(203, 251, 255, 1),
+                    endColor: Color.fromRGBO(81, 223, 234, 1),
+                    image: 'assets/sockes-two.png',
+                  ),
+                ],
               ),
             )
-          ],
+          ], // <Widget>[]
         ),
       ),
     );
   }
 }
+
+
